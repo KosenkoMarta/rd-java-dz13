e40a19eca54fe8824aa7b4b7a51557667b3394ed
KosenkoMarta <ingressjimenez@gmail.com>
2023-06-11 22:52:46 +0300

    Test data provider from DB

--- src/test/java/dataProviders/TestDataProviderFromDB.java
+++ src/test/java/dataProviders/TestDataProviderFromDB.java
@@ -8,7 +8,7 @@ public class TestDataProviderFromDB {
     @DataProvider(name = "persons")
     public static Object[][] personsFromDB() {
 
-        return DBReader.getPersonsFromDB().stream().map(person -> new Object[]{person.getFirstName(),
+        return DBReader.getPersonsFromDB().stream().map(person -> new Object[]{person.getId(), person.getFirstName(),
                 person.getLastName(), person.getAge(), person.getGender()}).toArray(Object[][]::new);
     }
 }
